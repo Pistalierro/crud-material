a4ec3ad815bdb1f6be6bb982d6febdaca3f3fe04
Anatoliy Sysoienko <pistalierro@gmail.com>
2026-02-23 20:49:18 +0200

    refresh API key of firestore

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,25 +1,16 @@
-import {ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection} from '@angular/core';
-import {provideRouter} from '@angular/router';
-import {provideFirebaseApp} from '@angular/fire/app';
-import {getFirestore, provideFirestore} from '@angular/fire/firestore';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { initializeApp } from 'firebase/app';
 
-import {routes} from './app.routes';
-
-import {initializeApp} from 'firebase/app';
-
-const firebaseConfig = {
-  apiKey: 'AIzaSyBLL5qqcWT8xFvbi2NYfxQhSUyonj13yzI',
-  authDomain: 'crud-material-d7289.firebaseapp.com',
-  projectId: 'crud-material-d7289',
-  storageBucket: 'crud-material-d7289.firebasestorage.app',
-  messagingSenderId: '428040604972',
-  appId: '1:428040604972:web:ece17f2ac114a39f6a3089'
-};
+import { firebaseConfig } from '../environments/firebase.config.local';
+import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideZoneChangeDetection({eventCoalescing: true}),
+    provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideFirebaseApp(() => initializeApp(firebaseConfig)),
     provideFirestore(() => getFirestore()),
---
+++ src/environments/firebase.config.example.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: 'REPLACE_ME',
+  authDomain: 'crud-material-d7289.firebaseapp.com',
+  projectId: 'crud-material-d7289',
+  storageBucket: 'crud-material-d7289.firebasestorage.app',
+  messagingSenderId: '428040604972',
+  appId: '1:428040604972:web:ece17f2ac114a39f6a3089',
+};
