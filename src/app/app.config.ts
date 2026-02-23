cbc8e0d6526ae6e853c47dcac8d45e9cd6dd4f40
Anatoliy Sysoienko <pistalierro@gmail.com>
2026-02-23 19:14:19 +0200

    init

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,27 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection} from '@angular/core';
+import {provideRouter} from '@angular/router';
+import {provideFirebaseApp} from '@angular/fire/app';
+import {getFirestore, provideFirestore} from '@angular/fire/firestore';
 
-import { routes } from './app.routes';
+import {routes} from './app.routes';
+
+import {initializeApp} from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyD5Tk4IFP2RfYDRLK7szQqndZcorM82M-k',
+  authDomain: 'crud-material-d7289.firebaseapp.com',
+  projectId: 'crud-material-d7289',
+  storageBucket: 'crud-material-d7289.firebasestorage.app',
+  messagingSenderId: '428040604972',
+  appId: '1:428040604972:web:ece17f2ac114a39f6a3089'
+};
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
-  ]
+    provideZoneChangeDetection({eventCoalescing: true}),
+    provideRouter(routes),
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideFirestore(() => getFirestore()),
+  ],
 };
