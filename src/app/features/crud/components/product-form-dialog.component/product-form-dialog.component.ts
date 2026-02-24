a39b52a19a7dfd807253240dd338d3fdb367afee
Anatoliy Sysoienko <pistalierro@gmail.com>
2026-02-24 13:08:09 +0200

    добавил файловую структуру + файлы

---
+++ src/app/features/crud/components/product-form-dialog.component/product-form-dialog.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-product-form-dialog',
+  imports: [],
+  templateUrl: './product-form-dialog.component.html',
+  styleUrl: './product-form-dialog.component.scss',
+})
+export class ProductFormDialogComponent {
+
+}
